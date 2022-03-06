6c1a4ffee32a1632b76d4c938e4c97c133fc0b01
joel-dev88 <jrchacon10388@gmail.com>
2022-03-06 11:21:55 -0800

    first

---
+++ demo-apps/nodejsapp/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+npm install
+
+# For the love of all that is good, don't use this in production
+# This is only for a demonstration of how things work behind the scene
+
+ssh vagrant@192.168.63.11 'sudo mkdir -p /app; sudo chown -R vagrant. /app'
+rsync -avz ./ vagrant@192.168.63.11:/app/
+ssh vagrant@192.168.63.11 "sudo systemctl restart nodeapp"
+
+
+ssh vagrant@192.168.63.12 'sudo mkdir -p /app; sudo chown -R vagrant. /app'
+rsync -avz ./ vagrant@192.168.63.12:/app/
+ssh vagrant@192.168.63.12 "sudo systemctl restart nodeapp"
